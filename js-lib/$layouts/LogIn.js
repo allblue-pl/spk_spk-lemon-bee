dbb485797bf5a7b0064cfcbaa83a2302312fcae1
Magda <m.golec@allblue.pl>
2018-06-09 13:31:15 +0200

    bootstrap 4

--- js-lib/$layouts/LogIn.js
+++ js-lib/$layouts/LogIn.js
@@ -7,7 +7,7 @@ const
 export default class LogIn extends spocky.Layout {
 
     static get Content() {
-        return [["div",{"class":["mg-fill-screen lb-background-login"]}],["div",{"class":["magda-holder"]},["div",{"class":["col-lg-5 col-sm-6 col-sm-8 lb-login"]},["div",{"class":["lb-login-content"]},["div",{"class":["lb-login-logo col-sm-5 col-sm-5 "]},["img",{"src":["$lb.images.logo"]}],["div",{"class":["mg-clear"]}]],["div",{"class":["lb-login-form col-sm-7 col-sm-7"]},["h3",{},"$lb.text('texts_logInMessage')"],["_",{"_holder":["form"]}]],["div",{"class":["mg-clear"]}]],["div",{"class":["lb-povered-by"]},"Powered by",["a",{"href":["https://allblue.pl"],"class":["text-primary"]},"AllBlue"]]]]];
+        return [["div",{"class":["mg-fill-screen lb-background-login"]}],["div",{"class":["magda-holder"]},["div",{"class":["col-xl-5 col-sm-8 lb-login"]},["div",{"class":["lb-login-content row"]},["div",{"class":["lb-login-logo col-md-5"]},["img",{"src":["$lb.images.logo"]}],["div",{"class":["mg-clear"]}]],["div",{"class":["lb-login-form col-md-7"]},["h3",{},"$lb.text('texts_logInMessage')"],["_",{"_holder":["form"]}]],["div",{"class":["mg-clear"]}]],["div",{"class":["lb-povered-by"]},"Powered by  ",["a",{"href":["https://allblue.pl"]},"AllBlue"]]]]];
     }
 
 
--- js-lib/$layouts/TopMenu.js
+++ js-lib/$layouts/TopMenu.js
@@ -7,7 +7,7 @@ const
 export default class TopMenu extends spocky.Layout {
 
     static get Content() {
-        return [["ul",{"class":[]},["li",{},["a",{"href":["/"]},["i",{"class":["fa fa-home"],"aria-hidden":["true"]}]]],["li",{"_repeat":["menuItems:item"],"class":["$item.activeClass"]},["a",{"href":["$item.uri"]},"$item.title"]]],["div",{"class":["mg-clear"]}]];
+        return [["div",{"class":["lb-menu-top"]},["nav",{"class":["navbar navbar-expand-md navbar-light"]},["button",{"class":["navbar-toggler"],"type":["button"],"data-toggle":["collapse"],"data-target":["#collapsibleNavbar"]},["span",{"class":["navbar-toggler-icon"]}]],["div",{"class":["collapse navbar-collapse"],"id":["collapsibleNavbar"]},["ul",{"class":["navbar-nav"]},["li",{"class":["nav-item"]},["a",{"class":["nav-link"],"href":["/"]},["i",{"class":["fa fa-home"],"aria-hidden":["true"]}]]],["li",{"_repeat":["menuItems:item"],"class":["nav-item ","$item.activeClass"]},["a",{"class":["nav-link"],"href":["$item.uri"]},"$item.title"]]]]]]];
     }
 
 
