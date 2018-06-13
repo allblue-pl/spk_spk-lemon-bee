191802b5ce1448ec74dd414977bd9e9a4086d4ae
SfTd <j.zolcik@allblue.pl>
2018-06-13 13:38:51 +0200

    Development.

--- js-lib/$layouts/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export const Body = require('./Body');
-export const LogIn = require('./LogIn');
-export const LogIn_Form = require('./LogIn_Form');
-export const Main = require('./Main');
-export const Panel = require('./Panel');
-export const TopMenu = require('./TopMenu');
-export const UserInfo = require('./UserInfo');
